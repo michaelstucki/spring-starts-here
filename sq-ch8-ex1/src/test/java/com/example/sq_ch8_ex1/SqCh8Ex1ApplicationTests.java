6448025e7c7c586729da14710af867c59082652a
michaelstucki <mstucki7@icloud.com>
2025-10-17 13:29:46 -0600

    first commit

---
+++ sq-ch8-ex1/src/test/java/com/example/sq_ch8_ex1/SqCh8Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sq_ch8_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh8Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
