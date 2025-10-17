728bddb73f38e5224abbfd1e97adca59bbeb1532
michaelstucki <mstucki7@icloud.com>
2025-10-17 16:57:28 -0600

    complete exercise

--- sq-ch8-ex1/src/main/java/com/example/sq_ch8_ex1/Main.java
+++ sq-ch8-ex1/src/main/java/com/example/sq_ch8_ex1/Main.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqCh8Ex1Application {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqCh8Ex1Application.class, args);
+
+		SpringApplication.run(Main.class, args);
 	}
 
 }
---
+++ sq-ch8-ex1/src/main/java/com/example/sq_ch8_ex1/MainController.java
@@ -0,0 +1,17 @@
+package com.example.sq_ch8_ex1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color, Model page) {
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", color);
+        return "home.html";
+    }
+}
